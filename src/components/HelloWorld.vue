1e58f962fa4b51b4a9b51a9edddd05f4064b524b
waseem-ishaq <wkinglastx@gmail.com>
2022-12-21 13:22:24 +0500

    ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,86 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div >
+    <v-row>
+      <v-col ><h1>Make your url shorter</h1></v-col>
+      
+    </v-row>
+    <v-row>
+      <v-col><hr></v-col>
+    </v-row>
+    <v-form>
+      <v-container class="ma-4">
+        <v-row>
+          <v-col cols="12">
+            <v-text-field
+              v-model="userUrl"
+              outlined
+              clearable
+              label="Enter your long url"
+              type="text"
+              ><template v-slot:append-outer>
+                <v-btn @click="apiCall(userUrl)"> convert </v-btn>
+              </template>
+            </v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+    <v-form>
+      <v-container class="ma-4">
+        <v-row>
+          <v-col cols="12">
+            <v-text-field
+              v-model="shorterUrl"
+              outlined
+              type="text"
+              placeholder="Your Short URL"
+              />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+
+   
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data: () => ({ userUrl: "",
+  shorterUrl:''
+ }),
+  methods: {
+    apiCall(userUrl) {
+      const url = new URL("https://t.ly/api/v1/link/shorten");
+      const params = {
+        api_token:
+          "FE5qNilsGxXqNaoOe2h0Iduz6kP1Bj4765AMqHHlKFbPLteG2DCtDutIHYoa",
+      };
+      Object.keys(params).forEach((key) =>
+        url.searchParams.append(key, params[key])
+      );
+      const headers = {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      };
+      let body = {
+        long_url: userUrl,
+        domain: "",
+        include_qr_code: false,
+      };
+      fetch(url, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(body),
+      }).then((response) => response.json().then((data)=>{
+        console.log(data.short_url);
+        this.shorterUrl=data.short_url;
+      }));
+    },
+  },
+};
+</script>
+<!-- FE5qNilsGxXqNaoOe2h0Iduz6kP1Bj4765AMqHHlKFbPLteG2DCtDutIHYoa -->
